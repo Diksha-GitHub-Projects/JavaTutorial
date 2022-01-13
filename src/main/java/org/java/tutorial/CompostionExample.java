1a751caf8719814832ca4056f21c0ac07011073a
Diksha Yadav <dikshayadav2009@gmail.com>
2022-01-13 10:36:53 +0530

    FunctionalProgramming in Java

--- src/main/java/org/java/tutorial/CompostionExample.java
+++ src/main/java/org/java/tutorial/CompostionExample.java
@@ -13,16 +13,12 @@ public class CompostionExample {
 
         Optional<Integer> naturalOrder = intData.stream().collect(Collectors.maxBy(Comparator.naturalOrder()));
         Optional<String> reverseOrder = data.stream().collect(Collectors.maxBy(String::compareTo));
-
-
         System.out.println(naturalOrder.orElse(0));
         System.out.println(reverseOrder.orElse("NAN"));
 
 
-
         Function<String,String> x1 = x -> x.toUpperCase();
         Function<String,String> x2 = x -> x + "_";
-
         Function<String,String>  x3 = x1.andThen(x2);
 
         data.stream().map(x3).collect(Collectors.toSet()).stream().forEach(System.out::println);
